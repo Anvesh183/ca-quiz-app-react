3da2b4257d0a3c13811213b86227fe852f2c1460
anvesh <anveshbachu4947@gmail.com>
2025-09-28 15:55:04 +0530

    change in loading text

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,9 @@ import React from "react";
 const Loader = ({ type = "" }) => (
   <div id="loader" className="flex flex-col justify-center items-center p-8">
     <div className={`loader-inner ${type}`}></div>
-    <p className="loader-text mt-4 italic text-gray-400">Loading...</p>
+    <p className="loader-text mt-4 italic text-gray-400">
+      Waking up the server... this might take a moment
+    </p>
   </div>
 );
 
